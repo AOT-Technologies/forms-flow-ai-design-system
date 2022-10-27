b08c58cc41ab7829e1a526ab8b635b0a8358bdb7
shuhaib-aot <shuhaib.s@aot-technologies.com>
2022-10-27 19:14:03 +0530

    updated class name

--- src/stories/components/Button.js
+++ src/stories/components/Button.js
@@ -1,12 +1,12 @@
 
 export const createButton = ({
-  size = 'forms-flow-btn-md',
-  type = 'forms-flow-btn-primary',
+  size = 'formsflow-btn-md',
+  type = 'formsflow-btn-primary',
   label,
 }) => {
   const btn = document.createElement('button');
   btn.type = 'button';
   btn.innerText = label;
-  btn.className = ['forms-flow-btn',size, type].join(' ');
+  btn.className = ['formsflow-btn',size, type].join(' ');
   return btn;
 };
